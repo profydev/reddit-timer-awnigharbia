9e9a063388d56dfc76c119f9433a1376fec8cc1f
Awni Gharbia <anongharbia@gmail.com>
2022-04-14 03:14:14 +0300

    Add header with acceptance criteria: - The logo is SVG - The logo refers to the home link `/` - The `How it works` and `about` links are URL hashes to the Homepage   `/` - The search link points to another route `/search` which renders   another components

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Logo from '../assets/logo.svg';
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 41px 80px;
+`;
+
+const Img = styled.img`
+  display: block;
+`;
+
+const StyledNav = styled.nav`
+  a {
+    margin-left: 26px;
+    text-decoration: none;
+    color: #636363;
+  }
+`;
 
 export default function Header() {
   return (
-    <div>
-      <h1>App Placeholder</h1>
-      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
-        <Link to="/">Home</Link>
+    <StyledHeader>
+      <Link to="/">
+        <Img src={Logo} />
+      </Link>
+      <StyledNav>
         <Link to="/search">Search</Link>
-      </nav>
-    </div>
+        <Link to="/#how-it-works">How it works</Link>
+        <Link to="/#about">About</Link>
+      </StyledNav>
+    </StyledHeader>
   );
 }
